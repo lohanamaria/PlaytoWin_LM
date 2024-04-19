ea00450f1fbc39f1cb6ab0d51d7a84c20f09c517
maria maria <decarvalholohana@gmail.com>
2024-04-18 21:57:41 -0400

    express

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const conn = require ("./db/conn");
 
 const Usuario = require("./models/Usuario");
 
+const express = require("express");
+app.use(
+
+    express.unlercoad({
+        extended:true,
+    })
+);
+app.use(express.json());
+
+const app = express();
+
+app.get("/usuarios/novo", (req, res) => {
+res.sendFile(`${__dirname}/views/formUsuario.html`);
+});
+
 conn.
 sync()
 then(() => {
