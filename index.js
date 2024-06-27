9f30b4e451d3182baaf88ac684b6831244c3e128
maria <116275294+lohanamaria@users.noreply.github.com>
2024-06-27 13:17:54 +0000

    formulario e rota para exclusao

--- index.js
+++ index.js
@@ -1,101 +1,123 @@
 require("dotenv").config();
-const conn = require ("./db/conn");
-
-const Usuario = require('./models/usuario');
-const Jogo = require('./models/Jogo');
-const Aquisicao = require('./models/Aquisicao');
-const Conquista = require('./models/Conquista');
+const express = require("express");
+const handlebars = require("express-handlebars");
+// const bodyParser = require("body-parser");
+const conn = require("./db/conn");
+const Usuario = require("./models/Usuario");
+// const Jogo = require("./models/Jogo"); // Se necessário, descomente e defina o modelo
+// const Aquisicao = require("./models/Aquisicao"); // Se necessário, descomente e defina o modelo
+// const Conquista = require("./models/Conquista"); // Se necessário, descomente e defina o modelo
 
 const app = express();
 
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 
-//app.use(express.unlercoad({unlercoad: true}));
-//app.use(express.json());
-//app.use("/")
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/usuarios/novo", (req, res) => {
+    res.render("formUsuario");
+});
+
+app.post("/usuarios/novo", async (req, res) => {
+    try {
+        const { nickname, nome } = req.body;
+        const usuario = await Usuario.create({ nickname, nome });
+        res.send("Usuário inserido sob o id " + usuario.id);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao inserir usuário.");
+    }
+});
+
+
+app.get("/usuarios", async (req, res) => {
+    try {
+        const usuarios = await Usuario.findAll({ raw: true });
+        res.render("usuarios", { usuarios });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao buscar usuários.");
+    }
+});
+
+app.get("/usuarios/:id/atualizar", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const usuario = await Usuario.findByPk(id, { raw: true });
+        res.render("formUsuario", { usuario });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao buscar usuário.");
+    }
+});
+
+app.post("/usuarios/:id/atualizar", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { nickname, nome } = req.body;
+        const registrosAfetados = await Usuario.update({ nickname, nome }, { where: { id } });
+        if (registrosAfetados > 0) {
+            res.redirect("/usuarios");
+        } else {
+            res.send("Erro ao atualizar usuário.");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao atualizar usuário.");
+    }
+});
+
+app.post("/usuarios/excluir", async (req, res) => {
+    try {
+        const id = req.body.id;
+        const registrosAfetados = await Usuario.destroy({ where: { id } });
+        if (registrosAfetados > 0) {
+            res.redirect("/usuarios");
+        } else {
+            res.send("Erro ao excluir usuário.");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro ao excluir usuário.");
+    }
+});
 
 
 app.get('/formJogo', (req, res) => {
-  res.sendFile(__dirname + '/views/formJogo.html');
+    res.sendFile(__dirname + '/views/formJogo.html');
 });
+
 
 app.post('/cadastrarJogo', async (req, res) => {
-  try {
-    const { titulo, descricao, precoBase } = req.body;
-    const jogo = await Jogo.create({
-      titulo,
-      descricao,
-      precoBase
-    });
-    res.redirect('/'); 
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro ao cadastrar o jogo.');
-  }
-}
+    try {
+        const { titulo, descricao, precoBase } = req.body;
+        const jogo = await Jogo.create({
+            titulo,
+            descricao,
+            precoBase
+        });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao cadastrar o jogo.');
+    }
+});
 
+app.listen(8000, () => {
+    console.log("Server rodando na porta 8000!");
+});
 
-const express = require("express");
-app.use(
-
-    express.unlercoad({
-        extended:true,
+conn.sync()
+    .then(() => {
+        console.log("Conectado e sincronizado!");
     })
-);
-app.use(express.json());
-
-const app = express();
-
-app.get("/usuarios/novo", (req, res) => {
-res.render(`${__dirname}´formUsuario`);
-});
-
-app.get("/", (req, res) => {
-res.render(`formJogo`);
-});
-
-app.post("/usuarios", async (req, res) =>
-  const usuarios  = await Usuario.findAll({raw:true})
-  res.render(´usuarios´, {usuarios});
-});
-
-
-app.post("usuarios/novo", async (req, res) => { 
-const nickname = req.body.nickname;
-const nome = reg.body.nome;
-
-const dadosUsuario = { 
-nickname,
-nome,
-
-  };
-const usuario = Usuario.create(dadosUsuario);
-
-res.send("Usuário inserido sob o id + usuario" + usuario.id);
-});
-
-app.get("/usuarios/:id/atualizar", (req, res) =>
-  { 
-    const id = req.params.id;
-    const usuario = Usuario.findByPk(id, {raw:true});
-    res.render("formUsuario", { usuario });   
-  } )
-
-app.listen(8000, () => { 
-console.log("Server rodando na porta 8000!");
-
-});
-
-conn.
-sync()
-then(() => {
-    console.log("Conectado e sincronizado!");
-})
-
-.catch((err) => {
-console.log("Erro!" + err) 
-
-})
+    .catch((err) => {
+        console.log("Erro!" + err);
+    });
--- models.js/Usuario.js
+++ models.js/Usuario.js
@@ -1,6 +1,15 @@
-const usuario = conn.define('usuario', {
-    nickname: { type: DataTypes.STRING },
-    nome: { type: DataTypes.STRING }
+const db = require('../db/connection');
+const { DataTypes } = require("sequelize");
+
+const Usuario = db.define("Usuario", {
+    nickname: {
+        type: DataTypes.STRING,
+        required: true,
+    },
+    nome: {
+        type: DataTypes.STRING,
+        required: true,
+    },
 });
 
-module.exports = usuario;
+module.exports =  Usuario;
